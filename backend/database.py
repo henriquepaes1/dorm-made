d8d0414b96f950cfea5942efb52a26cbf3323377
Nicolas Moreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2025-09-28 14:45:20 -0300

    integrating with supabase

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,12 @@
 from supabase import create_client
-from backend.config import SUPABASE_URL, SUPABASE_KEY
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
